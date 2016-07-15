c82e4c93731226018fe9aeb2d0ef950e3cd0f82f
Christine Lao <clao@ucsd.edu>
2016-07-14 18:53:24 -0700

    Added custom arrayadapter for movies

--- app/src/main/java/com/example/christine/flickster/MovieActivity.java
+++ app/src/main/java/com/example/christine/flickster/MovieActivity.java
@@ -2,7 +2,10 @@ package com.example.christine.flickster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.widget.ListView;
 
+import com.example.christine.flickster.adapters.MovieArrayAdapter;
 import com.example.christine.flickster.models.Movie;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -17,11 +20,19 @@ import cz.msebera.android.httpclient.Header;
 
 public class MovieActivity extends AppCompatActivity {
     ArrayList<Movie> movies;
+    MovieArrayAdapter movieAdapter;
+
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        lvItems = (ListView) findViewById(R.id.lvViews);
+        movies = new ArrayList<>();
+        movieAdapter = new MovieArrayAdapter(this, movies);
+        lvItems.setAdapter(movieAdapter);
 
         String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
@@ -35,7 +46,14 @@ public class MovieActivity extends AppCompatActivity {
 
                 try {
                     movieResults = response.getJSONArray("results");
-                    movies = Movie.fromJSONArray(movieResults);
+
+                    /** movies = Movie.fromJSONArray(movieResults);
+                     *  Changing reference of movie to be new block of data, don't track old movies
+                     *  Adapter is still tracking the old movies
+                     */
+
+                    movies.addAll(Movie.fromJSONArray(movieResults));
+                    movieAdapter.notifyDataSetChanged();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
